560e377b69bd0c82939a70fcb4911bac7ca8f7a6
Cory Borek <agentdid127cb@aol.com>
2026-02-07 10:25:07 -0500

    Generate Session ids

--- src/main/java/com/agentdid127/notiscanapi/api/impl/ClassManager.java
+++ src/main/java/com/agentdid127/notiscanapi/api/impl/ClassManager.java
@@ -2,9 +2,10 @@ package com.agentdid127.notiscanapi.api.impl;
 
 import com.agentdid127.notiscanapi.api.impl.account.AccountResource;
 import com.agentdid127.notiscanapi.api.impl.message.MessageResource;
+import com.agentdid127.notiscanapi.api.impl.session.SessionResource;
 
 public class ClassManager {
 
     // List of all classes in the api.
-    public static Class[] classes = {AccountResource.class, MessageResource.class};
+    public static Class[] classes = {AccountResource.class, MessageResource.class, SessionResource.class};
 }
