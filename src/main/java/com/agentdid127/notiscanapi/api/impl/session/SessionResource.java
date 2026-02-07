313b1ab30bec82ddf2cdfd5a5561c3388ae1d820
Cory Borek <agentdid127cb@aol.com>
2026-02-07 12:59:12 -0500

    fix limit of 1 message, Fetch all sessions tied to an owner

--- src/main/java/com/agentdid127/notiscanapi/api/impl/session/SessionResource.java
+++ src/main/java/com/agentdid127/notiscanapi/api/impl/session/SessionResource.java
@@ -1,6 +1,9 @@
 package com.agentdid127.notiscanapi.api.impl.session;
 
 import com.agentdid127.notiscanapi.NotiscanApiApplication;
+import com.erliapp.utilities.database.Database;
+import com.erliapp.utilities.database.DatabaseSelection;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -11,6 +14,7 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "sessionv1")
@@ -33,5 +37,26 @@ public class SessionResource {
         out.addProperty("id", sessionId);
         out.addProperty("owner", owner);
         return NotiscanApiApplication.GSON.toJson(out);
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("/all/{owner}")
+    public String getOwnerSessions(@PathParam("owner") long owner) throws IOException {
+       Database database = NotiscanApiApplication.DATABASE;
+       DatabaseSelection sel = database.select(new String[]{"session", "id"}, "message", "owner = " + owner);
+        JsonArray sessions = new JsonArray();
+       for (int i = 0; i < sel.size(); i++) {
+           boolean found = false;
+           for (int j = 0; j < sessions.size(); j++) {
+               if (sessions.get(j).getAsLong() == sel.getRow(i).get("session").getLong()) {
+                   found = true;
+               }
+           }
+           if (!found) {
+               sessions.add(sel.getRow(i).get("session").getLong());
+           }
+       }
+       return NotiscanApiApplication.GSON.toJson(sessions);
     }
 }
