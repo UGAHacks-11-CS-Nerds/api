c0a325a48037d0133469ef95d3cafb31bcc59a54
Yael Arroyo <yj29arroyo@gmail.com>
2026-02-07 11:10:20 -0500

    AccountResource Need to add with Database and check how AccountResource is created

--- src/main/java/com/agentdid127/notiscanapi/api/impl/account/AccountResource.java
+++ src/main/java/com/agentdid127/notiscanapi/api/impl/account/AccountResource.java
@@ -11,11 +11,73 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "accountv1")
 @Path("/account")
 public class AccountResource {
+
+    protected long id;
+    protected String username;
+    protected String email;
+    protected String salt;
+    protected String hash;
+
+    private static final String[] vars = new String[]{"id","username","email","salt","hash"};
+    private static final HashMap<String, AccountResource> accounts = new HashMap<String, AccountResource>();
+    /**
+     * Default Constructor
+     */
+    public AccountResource() {
+        this.id = 0;
+        this.username = "Unknown";
+        this.email = "Unknown";
+        this.salt = "Unknown";
+        this.hash = "Unknown";
+    }
+
+    /**
+     * Constructor with all values
+     * @param id id of message
+     * @param username Username of account
+     * @param email Email of account
+     * @param salt Random unique salt for account
+     * @param hash Unique hash for account
+     */
+    public AccountResource(long id, String username, String email, String salt, String hash) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.salt = salt;
+        this.hash = hash;
+    }
+
+    /**
+     * Default used during account creation
+     *
+     * @param username username provided by user
+     * @param email email provided by user
+     */
+    public AccountResource(String username, String email) {
+        this(NotiscanApiApplication.SNOWFLAKE.nextId(), username, email, createSalt(), "Unimplemented");
+        // accounts.put(username, this);
+    }
+
+    public static String createSalt() {
+        String alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        StringBuilder salt = new StringBuilder();
+        for (int i = 0; i < 16; ++i) {
+            int randInt = (int) (Math.random() * 48);
+            salt.append(alphabet.charAt(randInt));
+        }
+        // Check its a unique Salt
+
+        return salt.toString();
+    }
+//    public String createHash() {
+//        String randomhash =
+//    }
 
     /**
      * Retreives an account by id.
@@ -31,4 +93,41 @@ public class AccountResource {
         out.addProperty("id", id);
         return NotiscanApiApplication.GSON.toJson(out);
     }
+
+    public long getId() {return id;}
+
+    public String getUsername() {return username;}
+
+    public String getEmail() {return email;}
+
+    public String getSalt() {return salt;}
+
+    public String getHash() {return hash;}
+
+    public void setId(long id) {this.id = id;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setSalt(String salt) {this.salt = salt;}
+
+    public void setHash(String hash) {this.hash = hash;}
+
+    /**
+     * Called during the setup and returns email, password, and username
+     * @param email
+     * @param password
+     */
+    public void createLogin(String username, String email, String password) {
+        this.email = email;
+        this.username = username;
+        String intoHash = getSalt() + password;
+        setHash(String.valueOf(intoHash.hashCode()));
+    }
+
+//    void insertAccount() {
+//        NotiscanApiApplication.DATABASE.insert("");
+//    }
+
 }
